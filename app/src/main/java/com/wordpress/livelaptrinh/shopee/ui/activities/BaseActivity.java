a1008de1216889c68d4e627cf23945df89e8b712
Pham Van Thien <pvthiendeveloper@gmail.com>
2018-06-23 21:45:49 +0700

    Splash Screen

---
+++ app/src/main/java/com/wordpress/livelaptrinh/shopee/ui/activities/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.wordpress.livelaptrinh.shopee.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by MSI on 6/23/2018.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
