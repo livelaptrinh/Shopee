679d0a3da295426319014101bcdca642d20905f1
Pham Van Thien <pvthiendeveloper@gmail.com>
2018-06-23 21:53:25 +0700

    Splash Screen

--- app/src/main/java/com/wordpress/livelaptrinh/shopee/ui/splashscreen/SplashScreenAcitivity.java
+++ app/src/main/java/com/wordpress/livelaptrinh/shopee/ui/splashscreen/SplashScreenAcitivity.java
@@ -1,8 +1,10 @@
 package com.wordpress.livelaptrinh.shopee.ui.splashscreen;
 
+import com.wordpress.livelaptrinh.shopee.ui.activities.BaseActivity;
+
 /**
  * Created by MSI on 6/23/2018.
  */
 
-public class SplashScreenAcitivity {
+public class SplashScreenAcitivity extends BaseActivity {
 }
